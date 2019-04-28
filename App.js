17731b0f60accaf16704d3f1b5621c7e02252541
Nando <mnandohidayat@gmail.com>
2019-04-28 23:11:13 +0700

    BlickApp

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
 import { AppRegistry, View, Text } from 'react-native'
 
-class Greeting extends Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { isShowingText: true }
+
+    setInterval(() => {
+      this.setState(previousState => ({
+        isShowingText: !previousState.isShowingText
+      }))
+    }, 1000)
+  }
+
   render() {
-    return (
-      <View style={{ alignItems: 'center' }}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
-    )
+    if (!this.state.isShowingText) {
+      return null
+    }
+
+    return <Text>{this.props.text}</Text>
   }
 }
 
-export default class LotsOfGreetings extends Component {
+export default class BlinkApp extends Component {
   render() {
     return (
-      <View style={{ alignItems: 'center', top: 50 }}>
-        <Greeting name="Nando" />
-        <Greeting name="Sita" />
-        <Greeting name="Raissa" />
+      <View>
+        <Blink text="Whaaattt" />
+        <Blink text="This is awesome dude" />
       </View>
     )
   }
