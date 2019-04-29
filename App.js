0ce2022c2cdb71cee67f3329156d9bccf47a8668
Nando <mnandohidayat@gmail.com>
2019-04-29 10:34:46 +0700

    PizzaTranslator

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react'
-import { AppRegistry, View } from 'react-native'
+import { AppRegistry, View, TextInput, Text } from 'react-native'
 
-export default class AlignItemsBasics extends Component {
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+  }
+
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'stretch'
-        }}
-      >
-        <View
-          style={{ width: 50, height: 50, backgroundColor: 'powderblue' }}
+      <View style={{ padding: 10 }}>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here to translate!"
+          onChangeText={text => this.setState({ text })}
         />
-        <View style={{ height: 50, backgroundColor: 'skyblue' }} />
-        <View style={{ height: 100, backgroundColor: 'steelblue' }} />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text
+            .split(' ')
+            .map(word => word && '🍕')
+            .join(' ')}
+        </Text>
       </View>
     )
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => AlignItemsBasics)
+AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator)
