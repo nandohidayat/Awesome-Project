eb165cfa0e7f23a56c22875395eb3ae231e4d929
Nando <mnandohidayat@gmail.com>
2019-04-29 10:46:59 +0700

    ButtonBasics

--- App.js
+++ App.js
@@ -1,31 +1,46 @@
 import React, { Component } from 'react'
-import { AppRegistry, View, TextInput, Text } from 'react-native'
+import { AppRegistry, Alert, Button, StyleSheet, View } from 'react-native'
 
-export default class PizzaTranslator extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      text: ''
-    }
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
   }
 
   render() {
     return (
-      <View style={{ padding: 10 }}>
-        <TextInput
-          style={{ height: 40 }}
-          placeholder="Type here to translate!"
-          onChangeText={text => this.setState({ text })}
-        />
-        <Text style={{ padding: 10, fontSize: 42 }}>
-          {this.state.text
-            .split(' ')
-            .map(word => word && '🍕')
-            .join(' ')}
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.buttomContainer}>
+          <Button onPress={this._onPressButton} title="Press Me" />
+        </View>
+        <View style={styles.buttomContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button onPress={this._onPressButton} title="This looks great!" />
+          <Button onPress={this._onPressButton} title="OK!" color="#841584" />
+        </View>
       </View>
     )
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  buttomContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
+
+AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics)
