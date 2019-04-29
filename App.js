fcfe33c2df6405fe4f7ac6d14a8b7a92fcf480eb
Nando <mnandohidayat@gmail.com>
2019-04-29 10:00:40 +0700

    AlignItemsBasics

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react'
 import { AppRegistry, View } from 'react-native'
 
-export default class JustifyContentsBasics extends Component {
+export default class AlignItemsBasics extends Component {
   render() {
     return (
       <View
         style={{
           flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'space-between'
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'stretch'
         }}
       >
         <View
           style={{ width: 50, height: 50, backgroundColor: 'powderblue' }}
         />
-        <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
-        <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
+        <View style={{ height: 50, backgroundColor: 'skyblue' }} />
+        <View style={{ height: 100, backgroundColor: 'steelblue' }} />
       </View>
     )
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => JustifyContentsBasics)
+AppRegistry.registerComponent('AwesomeProject', () => AlignItemsBasics)
